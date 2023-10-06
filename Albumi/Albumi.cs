3d633211a033aaeab0b44ea626195861617fa141
Amos0000 <amosingman@icloud.com>
2023-10-06 11:33:31 +0300

    kiuas

---
+++ Albumi/Albumi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albumi
+{
+    internal class Class1
+    {
+    }
+}
